cd28fc6fda606b3e9fcdb2efeb5d7a7149c161ce
Danilkinkin <Danilkinkin@gmail.com>
2019-11-23 00:54:22 +0400

    Add ArrowBack, Widgets, Info and Category icons

--- src/app_res/js/Icons/Icons.js
+++ src/app_res/js/Icons/Icons.js
@@ -62,6 +62,25 @@ export class ArrowDown extends Icon{
 	}
 }
 
+export class ArrowBack extends Icon{
+	constructor(props){
+		super({
+			props: {
+				...props,
+				viewBox: "0 0 24 24",
+			},
+			icon: 
+			`
+			<path d="M19 11H7.83l4.88-4.88c.39-.39.39-1.03 0-1.42-.39-.39-1.02-.39-1.41 0l-6.59 6.59c-.39.39-.39 1.02 0 1.41l6.59 6.59c.39.39 1.02.39 1.41 0 .39-.39.39-1.02 0-1.41L7.83 13H19c.55 0 1-.45 1-1s-.45-1-1-1z"/>
+			`
+		});
+	}
+
+	static create(){
+		return new ArrowBack(...arguments);
+	}
+}
+
 export class Check extends Icon{
 	constructor(props){
 		super({
@@ -135,5 +154,64 @@ export class Toll extends Icon{
 
 	static create(){
 		return new Toll(...arguments);
+	}
+}
+
+export class Widgets extends Icon{
+	constructor(props){
+		super({
+			props: {
+				...props,
+				viewBox: "0 0 24 24",
+			},
+			icon: 
+			`
+			<path d="M13 14v6c0 .55.45 1 1 1h6c.55 0 1-.45 1-1v-6c0-.55-.45-1-1-1h-6c-.55 0-1 .45-1 1zm-9 7h6c.55 0 1-.45 1-1v-6c0-.55-.45-1-1-1H4c-.55 0-1 .45-1 1v6c0 .55.45 1 1 1zM3 4v6c0 .55.45 1 1 1h6c.55 0 1-.45 1-1V4c0-.55-.45-1-1-1H4c-.55 0-1 .45-1 1zm12.95-1.6L11.7 6.64c-.39.39-.39 1.02 0 1.41l4.25 4.25c.39.39 1.02.39 1.41 0l4.25-4.25c.39-.39.39-1.02 0-1.41L17.37 2.4c-.39-.39-1.03-.39-1.42 0z"/>
+			`
+		});
+	}
+
+	static create(){
+		return new Widgets(...arguments);
+	}
+}
+
+export class Info extends Icon{
+	constructor(props){
+		super({
+			props: {
+				...props,
+				viewBox: "0 0 24 24",
+			},
+			icon: 
+			`
+			<path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 15c-.55 0-1-.45-1-1v-4c0-.55.45-1 1-1s1 .45 1 1v4c0 .55-.45 1-1 1zm1-8h-2V7h2v2z"/>
+			`
+		});
+	}
+
+	static create(){
+		return new Info(...arguments);
+	}
+}
+
+export class Category extends Icon{
+	constructor(props){
+		super({
+			props: {
+				...props,
+				viewBox: "0 0 24 24",
+			},
+			icon: 
+			`
+			<path d="M11.15 3.4L7.43 9.48c-.41.66.07 1.52.85 1.52h7.43c.78 0 1.26-.86.85-1.52L12.85 3.4c-.39-.64-1.31-.64-1.7 0z"/>
+			<circle cx="17.5" cy="17.5" r="4.5"/>
+			<path d="M4 21.5h6c.55 0 1-.45 1-1v-6c0-.55-.45-1-1-1H4c-.55 0-1 .45-1 1v6c0 .55.45 1 1 1z"/>
+			`
+		});
+	}
+
+	static create(){
+		return new Category(...arguments);
 	}
 }
