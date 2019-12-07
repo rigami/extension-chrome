101cfeab500989024f999150937ff7cb1981e557
Danilkinkin <Danilkinkin@gmail.com>
2019-12-07 22:46:12 +0400

    Add '$classIndex' parameter

--- src/app_res/js/themes/style.js
+++ src/app_res/js/themes/style.js
@@ -52,19 +52,19 @@ export const defaultTheme = {
 let usedTheme = null;
 let stylesConstructor = null;
 
-let stylesList = [];
+let stylesList = {};
 const classesAlphabet = ["a", "b", "c", "d", "e", "f", "j", "h", "i", "g", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 let classesIndex = 0;
 let updateTimeout = null;
 let stableClassesStore = document.createElement("style");
-let tempClassesStore = document.createElement("style");
+//let tempClassesStore = document.createElement("style");
 
 export function setTheme(theme){
 	usedTheme = theme;
 	stylesConstructor = useTheme();
 
 	document.head.appendChild(stableClassesStore);
-	document.head.appendChild(tempClassesStore);
+	//document.head.appendChild(tempClassesStore);
 }
 
 export const useTheme = () => new function(){
@@ -85,33 +85,42 @@ export const useClasses = (styles) => {
 
 	Object.keys(styles).forEach(className => {
 		let calcStyle = "";
+		let classProps = {classIndex: 0};
 
 		Object.keys(styles[className]).forEach(style => {
-			let i = 0;
+			switch (style) {
+				case "$classIndex":
+					classProps.classIndex = styles[className][style];
+					break;
+				default:
+					let i = 0;
+					let lowerStyle = style;
 
-			let lowerStyle = style;
+					while(i < lowerStyle.length){
+						if(lowerStyle[i] === lowerStyle[i].toUpperCase()){
+							lowerStyle = lowerStyle.substring(0, i)+"-"+lowerStyle[i].toLowerCase()+lowerStyle.substring(i+1)
+							i++;
+						}
+						i++;
+					}
 
-			while(i < lowerStyle.length){
-				if(lowerStyle[i] === lowerStyle[i].toUpperCase()){
-					lowerStyle = lowerStyle.substring(0, i)+"-"+lowerStyle[i].toLowerCase()+lowerStyle.substring(i+1)
-					i++;
-				}
-				i++;
-			}
-
-			calcStyle += lowerStyle+":"+styles[className][style]+";";
+					calcStyle += lowerStyle+":"+styles[className][style]+";";
+					break;
+			}			
 		});
+
+		if(!stylesList[classProps.classIndex]) stylesList[classProps.classIndex] = [];
 
 		let i = 0;		
 
-		while(i < classesIndex && stylesList[i] !== calcStyle) i++;
+		while(i < stylesList[classProps.classIndex].length && stylesList[classProps.classIndex][i].style !== calcStyle) i++;
 
-		calcClasses[className] = getClassName(i);
-
-		if(i === classesIndex){
+		if(i === stylesList[classProps.classIndex].length){
+			stylesList[classProps.classIndex].push({style: calcStyle, index: classesIndex});
 			classesIndex++;
-			stylesList.push(calcStyle);
 		}
+
+		calcClasses[className] = getClassName(stylesList[classProps.classIndex][i].index);
 	});
 
 	updateStyles();
@@ -136,10 +145,14 @@ function getClassName(index = 0){
 function updateStyles(){
 	if(updateTimeout) clearTimeout(updateTimeout);
 
-	tempClassesStore.innerHTML = stylesList.map((style, index) => `.${getClassName(index)}{${style}}`).join("");
+	stableClassesStore.innerHTML = "";
 
-	updateTimeout = setTimeout(() => {
+	Object.keys(stylesList).forEach(index => {
+		stableClassesStore.innerHTML += stylesList[index].map((classObject, index) => `.${getClassName(classObject.index)}{${classObject.style}}`).join("");
+	});
+
+	/*updateTimeout = setTimeout(() => {
 		stableClassesStore.innerHTML = tempClassesStore.innerHTML;
 		//tempClassesStore.innerHTML = '';
-	}, 1000);
+	}, 1000);*/
 }
