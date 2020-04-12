90b05c31bfae72861d1d39ef6faa9d8ddbdcf6a2
Danilkinkin <danilkinkin@gmail.com>
2020-04-12 22:05:28 +0400

    Add manifest.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,50 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     context: path.resolve(__dirname, "src"),
     entry: './index.js',
     output: {
-        filename: '[name].bundle.js',
+        chunkFilename: '[id].bundle.js',
         path: path.resolve(__dirname, 'build'),
     },
     devtool: 'inline-source-map',
     devServer: {
-        contentBase: '/build',
+        contentBase: path.resolve(__dirname, 'public'),
         hot: true,
+        injectHot: (compilerConfig) => compilerConfig.name === 'only-include',
         open: false,
+        overlay: true,
+        writeToDisk: true,
+        disableHostCheck: true,
     },
     plugins: [
-        new CleanWebpackPlugin(),
+        new CleanWebpackPlugin({
+            cleanAfterEveryBuildPatterns: ['*.bundle.js', '*.hot-update.js', '!resource/*', '!manifest.json'],
+        }),
         new HtmlWebpackPlugin({
             template: "./index.html",
             filename: "index.html"
         }),
+        new CopyWebpackPlugin([
+            {
+                from: "./manifest.json",
+                to:   "./manifest.json",
+                transform (content, path) {
+                    return content;
+                }
+            },
+            {
+                from: path.resolve(__dirname, 'public/'),
+                to:   "./resource/",
+                transform (content, path) {
+                    console.log(path)
+                    return content;
+                }
+            }
+        ]),
     ],
     module: {
         rules: [
@@ -39,6 +63,7 @@ module.exports = {
             'react': 'preact/compat',
             'react-dom': 'preact/compat',
             'ui': '/ui',
+            'ui-components': '/ui-components',
             'i18n': '/i18n',
             'dict': '/dict',
             'config': '/config',
