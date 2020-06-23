3004dfe1fb7af82353015af8cb795020c81787e5
Danilkinkin <danilkinkin@gmail.com>
2020-06-23 17:32:42 +0400

    Added BeautifulFileSize

---
+++ src/utils/beautifulFileSize.js
@@ -0,0 +1,11 @@
+function BeautifulFileSize(bytes) {
+    const kb = bytes / 1024;
+
+    if (kb <= 1024) {
+        return [Math.round(kb), 'kb'];
+    }
+
+    return [Math.round(kb / 1024), 'mb'];
+}
+
+export default BeautifulFileSize;
