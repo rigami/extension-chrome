bbb757d31d8e8b9735c9a02444a1981d697d3697
Danilkinkin <danilkinkin@gmail.com>
2020-06-25 22:48:23 +0400

    Added site search

--- src/ui/Bookmarks/EditBookmarkModal/SearchFiled.jsx
+++ src/ui/Bookmarks/EditBookmarkModal/SearchFiled.jsx
@@ -8,79 +8,147 @@ import {
 } from '@material-ui/core';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import locale from '@/i18n/RU';
-import siteSearch, { getFaviconUrl } from "@/utils/siteSearch";
-import Autocomplete from "@material-ui/lab/Autocomplete";
+import siteSearch, { getFaviconUrl, AbortController } from "@/utils/siteSearch";
+import Autocomplete, { createFilterOptions } from "@material-ui/lab/Autocomplete";
 import { PublicRounded as WebSiteIcon } from '@material-ui/icons';
+import clsx from 'clsx';
 
 const { global: localeGlobal } = locale;
 
 const useStyles = makeStyles((theme) => ({
     favicon: {
-        color: theme.palette.text.secondary,
+        color: '#505050',
         marginRight: theme.spacing(2),
         width: 16,
         height: 16,
+        backgroundColor: theme.palette.common.white,
+        // border: `1px solid ${theme.palette.common.white}`,
+    },
+    input: {
+        padding: 0,
+        paddingRight: theme.spacing(2),
+    },
+    progressIcon: {
+        marginLeft: theme.spacing(2),
+    },
+    row: {
+        flexWrap: 'nowrap',
+    },
+    textBlock: {
+        overflow: 'hidden',
+    },
+    title: {
+        fontWeight: 700,
+        whiteSpace: 'nowrap',
+        overflow: 'hidden',
+        textOverflow: 'ellipsis',
+        width: '100%',
+        flexGrow: 0,
+    },
+    url: {
+        whiteSpace: 'nowrap',
+        overflow: 'hidden',
+        textOverflow: 'ellipsis',
     },
 }));
 
-function SearchField({ className: externalClassName, value, onChange }) {
+
+function SearchField({ className: externalClassName, value: defaultValue, onChange }) {
     const classes = useStyles();
     const [timer, setTimer] = useState(undefined);
-    const [open, setOpen] = React.useState(false);
     const [options, setOptions] = React.useState([]);
-    const loading = open && options.length === 0;
+    const [loading, setLoading] = React.useState(false);
+    const [controller, setController] = React.useState(null);
+    const [isOpen, setIsOpen] = React.useState(false);
+    const [value, setValue] = React.useState('');
+
+    const filterOptions = createFilterOptions({
+        stringify: () => value,
+    });
 
     const handleSearch = (event) => {
-        value = event.target.value;
+        setLoading(true);
+        const inputValue = event.target.value;
+        if (controller) {
+            controller.abort();
+            setController(null);
+        }
         if (timer) {
             clearTimeout(timer);
         }
 
+        if (inputValue.trim() === '') {
+            setOptions([]);
+            setLoading(false);
+            return;
+        }
+
         setTimer(setTimeout(() => {
             setTimer(null);
-            siteSearch(value)
+            const controller = new AbortController();
+            setController(controller);
+
+            siteSearch(inputValue, controller)
                 .then((results) => {
+                    console.log(`Find for ${inputValue}:`, results)
                     setOptions(results);
+                    setLoading(false);
+                    setIsOpen(true);
                 });
         }, 1300));
 
-        onChange(value);
+        setValue(inputValue);
     };
-
-    useEffect(() => {
-        if (!open) {
-            setOptions([]);
-        }
-    }, [open]);
 
     return (
         <Autocomplete
             fullWidth
-            open={open}
-            onOpen={() => {
-                setOpen(true);
-            }}
+            open={isOpen}
             onClose={() => {
-                setOpen(false);
+                setIsOpen(false);
+                setOptions([]);
+                setLoading(false);
+                if (controller) {
+                    controller.abort();
+                    setController(null);
+                }
+                if (timer) {
+                    clearTimeout(timer);
+                }
             }}
-            getOptionSelected={(option, value) => option.title === value.title}
-            getOptionLabel={(option) => option.title}
+            onChange={(option) => onChange(option.url)}
+            getOptionSelected={(option, value) =>
+                (typeof option === "string" ? option : option.title)
+                === (typeof value === "string" ? value : value.title)
+            }
+            getOptionLabel={(option) => option.url}
+            autoHighlight
+            noOptionsText={(
+                <Grid container alignItems="center">
+                    <Grid item xs>
+                        <span style={{ fontWeight: 700 }}>
+                            Ничего не найдено
+                        </span>
+                    </Grid>
+                </Grid>
+            )}
             renderOption={(option) => {
                 return (
-                    <Grid container alignItems="center">
+                    <Grid container alignItems="center" className={classes.row}>
                         <Grid item>
                             <Avatar
+                                key={option.url}
                                 src={getFaviconUrl(option.url)}
                                 className={classes.favicon}
                             >
                                 <WebSiteIcon />
                             </Avatar>
                         </Grid>
-                        <Grid item xs>
-                            <span style={{ fontWeight: 700 }}>
+                        <Grid item xs className={classes.textBlock}>
+                            <span style={{ fontWeight: 700 }} className={classes.title}>
                               {option.title}
                             </span>
-                            <Typography variant="body2" color="textSecondary">
+                            <Typography variant="body2" color="textSecondary" className={classes.url}>
                                 {option.url}
                             </Typography>
                         </Grid>
@@ -88,22 +156,38 @@ function SearchField({ className: externalClassName, value, onChange }) {
                 );
             }}
             options={options}
-            loading={loading}
+            filterOptions={filterOptions}
             renderInput={(params) => (
                 <TextField
                     {...params}
-                    label="URL адрес"
+                    label="Запрос или URL адрес"
                     variant="outlined"
                     className={externalClassName}
-                    value={value}
+                    value={defaultValue}
                     onChange={handleSearch}
+                    onBlur={() => {
+                        setIsOpen(false);
+                        setOptions([]);
+                        setLoading(false);
+                        if (controller) {
+                            controller.abort();
+                            setController(null);
+                        }
+                        if (timer) {
+                            clearTimeout(timer);
+                        }
+                    }}
                     InputProps={{
                         ...params.InputProps,
+                        className: classes.input,
                         endAdornment: (
-                            <React.Fragment>
-                                {loading ? <CircularProgress color="inherit" size={20} /> : null}
-                                {params.InputProps.endAdornment}
-                            </React.Fragment>
+                            loading && (
+                                <CircularProgress
+                                    color="inherit"
+                                    size={20}
+                                    className={classes.progressIcon}
+                                />
+                            )
                         ),
                     }}
                 />
--- src/utils/siteSearch.js
+++ src/utils/siteSearch.js
@@ -1,10 +1,14 @@
-const search = (query) => {
+const search = (query, signal) => {
     return new Promise((resolve, reject) => {
         let xhr = new XMLHttpRequest();
         xhr.open('GET', `http://localhost:8080/parser/proxy_DuckDuckGo?query=${encodeURI(query)}`, true);
         xhr.responseType = "document";
         xhr.timeout = 10000;
         xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+
+        if (signal) {
+            signal._abortHandler = () => xhr.abort();
+        }
 
         xhr.onload = () => {
             if (xhr.status !== 200) {
@@ -13,7 +17,7 @@ const search = (query) => {
             }
             resolve(Array.from(xhr.responseXML.body.querySelectorAll(".result-link")).map((link) => ({
                 url: link.href,
-                title: link.innerHTML,
+                title: link.innerHTML.replace(/<(b|span)[^>]*>|<\/(b|span)>/g, ""),
             })));
         }
 
@@ -36,6 +40,11 @@ const getFaviconUrl = (url) => {
     return `${origin}/favicon.ico`;
 };
 
+function AbortController() {
+    this.abort = () => this._abortHandler && this._abortHandler();
+    this._abortHandler = null;
+}
+
 
 export default search;
-export { getFaviconUrl };
+export { getFaviconUrl, AbortController };
