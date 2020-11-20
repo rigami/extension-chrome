c60c41b0a8afe85464bde1a687f03351dd5b31e2
Danilkinkin <danilkinkin@gmail.com>
2020-11-21 01:06:54 +0400

    Fixed upgrade db

--- src/config/db.js
+++ src/config/db.js
@@ -1,59 +1,131 @@
-export default {
-    upgrade(db) {
-        console.log('upgrade db');
-        const backgroundsStore = db.createObjectStore('backgrounds', {
+function upgradeOrCreateBackgrounds(db, transaction) {
+    let store;
+
+    if (db.objectStoreNames.contains('backgrounds')) {
+        store = transaction.objectStore('backgrounds');
+    } else {
+        store = db.createObjectStore('backgrounds', {
             keyPath: 'id',
             autoIncrement: true,
         });
-        backgroundsStore.createIndex('type', 'type', { unique: false });
-        backgroundsStore.createIndex('author', 'author', { unique: false });
-        backgroundsStore.createIndex('source_link', 'sourceLink', { unique: false });
-        backgroundsStore.createIndex('file_name', 'fileName', { unique: false });
+        store.createIndex('type', 'type', { unique: false });
+        store.createIndex('author', 'author', { unique: false });
+        store.createIndex('source_link', 'sourceLink', { unique: false });
+        store.createIndex('file_name', 'fileName', { unique: false });
+    }
 
-        const bookmarksStore = db.createObjectStore('bookmarks', {
+    return store;
+}
+
+function upgradeOrCreateBookmarks(db, transaction) {
+    let store;
+
+    if (db.objectStoreNames.contains('bookmarks')) {
+        store = transaction.objectStore('bookmarks');
+    } else {
+        store = db.createObjectStore('bookmarks', {
             keyPath: 'id',
             autoIncrement: true,
         });
-        bookmarksStore.createIndex('ico_variant', 'icoVariant', { unique: false });
-        bookmarksStore.createIndex('ico_file_name', 'icoFileName', { unique: false });
-        bookmarksStore.createIndex('url', 'url', { unique: false });
-        bookmarksStore.createIndex('name', 'name', { unique: false });
-        bookmarksStore.createIndex('description', 'description', { unique: false });
-        bookmarksStore.createIndex('count_clicks', 'countClicks', { unique: false });
-        bookmarksStore.createIndex('folder_id', 'folderId', { unique: false });
+        store.createIndex('ico_variant', 'icoVariant', { unique: false });
+        store.createIndex('ico_file_name', 'icoFileName', { unique: false });
+        store.createIndex('url', 'url', { unique: false });
+        store.createIndex('name', 'name', { unique: false });
+        store.createIndex('description', 'description', { unique: false });
+        store.createIndex('count_clicks', 'countClicks', { unique: false });
+        store.createIndex('folder_id', 'folderId', { unique: false });
+    }
 
-        const categoriesStore = db.createObjectStore('categories', {
+    if (!store.indexNames.contains("folder_id")) {
+        store.createIndex('folder_id', 'folderId', { unique: false });
+    }
+
+    return store;
+}
+
+function upgradeOrCreateCategories(db, transaction) {
+    let store;
+
+    if (db.objectStoreNames.contains('categories')) {
+        store = transaction.objectStore('categories');
+    } else {
+        store = db.createObjectStore('categories', {
             keyPath: 'id',
             autoIncrement: true,
         });
-        categoriesStore.createIndex('name', 'name', { unique: false });
-        categoriesStore.createIndex('color', 'color', { unique: true });
+        store.createIndex('name', 'name', { unique: false });
+        store.createIndex('color', 'color', { unique: true });
+    }
 
-        const bByCStore = db.createObjectStore('bookmarks_by_categories', {
+    return store;
+}
+
+function upgradeOrCreateBookmarksByCategories(db, transaction) {
+    let store;
+
+    if (db.objectStoreNames.contains('bookmarks_by_categories')) {
+        store = transaction.objectStore('bookmarks_by_categories');
+    } else {
+        store = db.createObjectStore('bookmarks_by_categories', {
             keyPath: 'id',
             autoIncrement: true,
         });
-        bByCStore.createIndex('category_id', 'categoryId', { unique: false });
-        bByCStore.createIndex('bookmark_id', 'bookmarkId', { unique: false });
+        store.createIndex('category_id', 'categoryId', { unique: false });
+        store.createIndex('bookmark_id', 'bookmarkId', { unique: false });
+    }
 
-        const foldersStore = db.createObjectStore('folders', {
+    return store;
+}
+
+function upgradeOrCreateFolders(db, transaction) {
+    let store;
+
+    if (db.objectStoreNames.contains('folders')) {
+        store = transaction.objectStore('folders');
+    } else {
+        store = db.createObjectStore('folders', {
             keyPath: 'id',
             autoIncrement: true,
         });
-        foldersStore.createIndex('name', 'name', { unique: false });
-        foldersStore.createIndex('parent_id', 'parentId', { unique: false });
+        store.createIndex('name', 'name', { unique: false });
+        store.createIndex('parent_id', 'parentId', { unique: false });
 
-        foldersStore.add({
+        store.add({
             name: 'rigami',
             parentId: 0,
         });
+    }
 
-        const favoritesStore = db.createObjectStore('favorites', {
+    return store;
+}
+
+function upgradeOrCreateFavorites(db, transaction) {
+    let store;
+
+    if (db.objectStoreNames.contains('favorites')) {
+        store = transaction.objectStore('favorites');
+    } else {
+        store = db.createObjectStore('favorites', {
             keyPath: 'id',
             autoIncrement: true,
         });
-        favoritesStore.createIndex('favorite_id', 'favoriteId', { unique: false });
-        favoritesStore.createIndex('type', 'type', { unique: false });
+        store.createIndex('favorite_id', 'favoriteId', { unique: false });
+        store.createIndex('type', 'type', { unique: false });
+    }
+
+    return store;
+}
+
+
+export default {
+    upgrade(db, oldVersion, newVersion, transaction) {
+        console.log('upgrade db', db, transaction);
+        upgradeOrCreateBackgrounds(db, transaction);
+        upgradeOrCreateBookmarks(db, transaction);
+        upgradeOrCreateCategories(db, transaction);
+        upgradeOrCreateBookmarksByCategories(db, transaction);
+        upgradeOrCreateFolders(db, transaction);
+        upgradeOrCreateFavorites(db, transaction);
     },
     blocked() {},
     blocking() {},
