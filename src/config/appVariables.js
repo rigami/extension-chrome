4cd2ce3ab2bef97ac3833b4c9c4d9bd55898357b
Danilkinkin <danilkinkin@gmail.com>
2020-11-21 02:34:37 +0400

    Change default bg

--- src/config/appVariables.js
+++ src/config/appVariables.js
@@ -9,11 +9,11 @@ export default {
     fs: { root: `chrome-extension://${chrome.runtime.id}/persistent/` },
     maxUploadFiles: 15,
     defaultBG: {
-        src: `${PRODUCTION_MODE ? 'https://api.rigami.io' : 'http://localhost:8080'}/jeremy-bishop-hVkDaLkoiec-unsplash.jpg`,
-        author: 'Jeremy Bishop',
+        src: `${PRODUCTION_MODE ? 'https://api.rigami.io' : 'http://localhost:8080'}/pika-alyani-WajET_vzPmI-unsplash.jpg`,
+        author: 'Pika Alyani',
         type: BG_TYPE.IMAGE,
         description: 'Unsplash photo',
-        sourceLink: 'https://unsplash.com/photos/hVkDaLkoiec',
+        sourceLink: 'https://unsplash.com/photos/WajET_vzPmI',
     },
     rest: { url: `${PRODUCTION_MODE ? 'https://api.rigami.io' : 'http://localhost:8080'}` },
 };
