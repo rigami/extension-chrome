741e9824d3401d5a6b49304edb40bd137205e7c4
Danilkinkin <danilkinkin@gmail.com>
2020-12-09 00:56:39 +0400

    Change gird

--- src/ui/Menu/Settings/Backgrounds/Library/index.jsx
+++ src/ui/Menu/Settings/Backgrounds/Library/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, memo } from 'react';
-import { BG_TYPE } from '@/enum';
+import { BG_TYPE, FETCH } from '@/enum';
 import {
     Box,
     Avatar,
@@ -8,12 +8,18 @@ import {
     Tooltip,
     CircularProgress,
     Typography,
+    GridList,
+    GridListTile,
+    ListSubheader,
+    GridListTileBar,
+    Link,
 } from '@material-ui/core';
 import {
     WallpaperRounded as WallpaperIcon,
     CloudDownloadRounded as GetFromLibraryIcon,
     DeleteForeverRounded as DeleteIcon,
     CheckRounded as SetIcon,
+    ArrowForwardRounded as LeftIcon,
 } from '@material-ui/icons';
 import { fade } from '@material-ui/core/styles/colorManipulator';
 import FSConnector from '@/utils/fsConnector';
@@ -24,8 +30,13 @@ import { observer } from 'mobx-react-lite';
 import FullscreenStub from '@/ui-components/FullscreenStub';
 import useBackgroundsService from '@/stores/BackgroundsStateProvider';
 import LoadBGFromLocalButton from './LoadBGFromLocalButton';
+import { last } from 'lodash';
+import useCoreService from '@/stores/BaseStateProvider';
 
 const useStyles = makeStyles((theme) => ({
+    root: {
+        overflow: 'hidden',
+    },
     bgWrapper: {
         display: 'flex',
         flexWrap: 'wrap',
@@ -42,9 +53,6 @@ const useStyles = makeStyles((theme) => ({
         width: '100%',
     },
     bgCardWrapper: {
-        paddingRight: theme.spacing(0.5),
-        paddingBottom: theme.spacing(0.5),
-        flexBasis: '25%',
         height: theme.spacing(20),
     },
     bgActionsWrapper: {
@@ -54,13 +62,14 @@ const useStyles = makeStyles((theme) => ({
         right: 0,
         bottom: 0,
         display: 'flex',
+        flexDirection: 'column',
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'flex-end',
         backgroundColor: fade(theme.palette.common.black, 0.7),
         opacity: 0,
         transition: theme.transitions.create('', {
             easing: theme.transitions.easing.easeInOut,
-            duration: theme.transitions.duration.short,
+            duration: theme.transitions.duration.shortest,
         }),
         '&:hover': { opacity: 1 },
     },
@@ -103,6 +112,30 @@ const useStyles = makeStyles((theme) => ({
         height: '100%',
         width: '100%',
     },
+    bgDescription: {
+
+    },
+    titleBar: {
+        position: 'relative',
+        width: '100%',
+        height: 'auto',
+        padding: theme.spacing(1, 0),
+        paddingRight: theme.spacing(2),
+        background: 'none',
+        borderTop: `1px solid ${fade(theme.palette.common.white, 0.12)}`,
+    },
+    icon: {
+        display: 'flex',
+        color: theme.palette.common.white,
+    },
+    link: {
+        position: 'relative',
+        width: '100%',
+    },
+    selectIcon: {
+        backgroundColor: theme.palette.common.white,
+        color: theme.palette.common.black,
+    },
 }));
 
 const headerProps = {
@@ -133,94 +166,116 @@ function HeaderActions() {
     );
 }
 
-function BGCard({ fileName, onSet, onRemove }) {
-    const classes = useStyles();
-    const { t } = useTranslation();
-
-    return (
-        <div className={classes.bgCardWrapper}>
-            <Box
-                className={classes.bgCard}
-                style={{ backgroundImage: `url('${FSConnector.getBGURL(fileName, 'preview')}')` }}
-            >
-                <Avatar variant="square" className={classes.bgStub}>
-                    <WallpaperIcon fontSize="large" />
-                </Avatar>
-                <Box className={classes.bgActionsWrapper}>
-                    <Tooltip title={t('settings.bg.apply')}>
-                        <Button className={classes.setIcon} onClick={onSet}>
-                            <SetIcon />
-                        </Button>
-                    </Tooltip>
-                    <Tooltip title={t('bg.remove')}>
-                        <IconButton className={classes.deleteIcon} onClick={onRemove}>
-                            <DeleteIcon />
-                        </IconButton>
-                    </Tooltip>
-                </Box>
-            </Box>
-        </div>
-    );
-}
-
-const MemoBGCard = memo(BGCard);
-
 function LibraryMenu() {
     const backgroundsService = useBackgroundsService();
+    const coreService = useCoreService();
     const { t } = useTranslation();
     const classes = useStyles();
 
     const [bgs, setBgs] = useState(null);
-    const [state, setState] = useState('pending');
+    const [state, setState] = useState(FETCH.PENDING);
 
     useEffect(() => {
         backgroundsService.getAll()
             .then((values) => {
-                setBgs(values);
-                setState('done');
+                const groups = values.reduce((acc, bg) => {
+                    let group = acc.find(({ type }) => type === bg.type);
+                    if (!group) {
+                        acc.push({
+                            type: bg.type,
+                            list: [],
+                        });
+                        group = last(acc);
+                    }
+                    group.list.push(bg);
+                    return acc;
+                }, []);
+
+                setBgs(groups);
+                setState(FETCH.DONE);
             })
             .catch((e) => {
-                setState('failed');
+                setState(FETCH.FAILED);
                 console.error('Failed load bg`s from db:', e);
             });
     }, [backgroundsService.count]);
 
     return (
-        <React.Fragment>
-            {state === 'pending' && (
+        <Box className={classes.root}>
+            {state === FETCH.PENDING && (
                 <Box className={classes.centerPage}>
                     <CircularProgress />
                 </Box>
             )}
-            {
-                state === 'done' && Object.keys(BG_TYPE)
-                    .filter((BGType) => bgs.filter(({ type }) => type === BG_TYPE[BGType]).length > 0)
-                    .map((BGType) => (
-                        <React.Fragment key={BGType}>
-                            <SectionHeader title={t(`settings.bg.general.library.type.${BG_TYPE[BGType]}`)} />
-                            <Box className={classes.bgWrapper}>
-                                {bgs.filter(({ type }) => type === BG_TYPE[BGType]).map((bg) => (
-                                    <MemoBGCard
-                                        {...bg}
-                                        key={bg.id}
-                                        onSet={() => backgroundsService.setCurrentBG(bg.id)}
-                                        onRemove={() => backgroundsService.removeFromStore(bg.id)}
-                                    />
-                                ))}
-                            </Box>
-                        </React.Fragment>
-                    ))
-            }
-            {state === 'done' && bgs.length === 0 && (
+            {state === FETCH.DONE && bgs.length !== 0 && (
+                <GridList cellHeight={160} cols={4}>
+                    {bgs.map((group) => [
+                        (
+                            <GridListTile cols={4} style={{ height: 'auto' }}>
+                                <ListSubheader component="div">
+                                    {t(`settings.bg.general.library.type.${group.type}`)}
+                                </ListSubheader>
+                            </GridListTile>
+                        ),
+                        ...group.list.map((bg) => (
+                            <GridListTile key={bg.id} className={classes.bgCardWrapper}>
+                                <Box
+                                    className={classes.bgCard}
+                                    style={{ backgroundImage: `url('${FSConnector.getBGURL(bg.fileName, 'preview')}')` }}
+                                >
+                                    <Avatar variant="square" className={classes.bgStub}>
+                                        <WallpaperIcon fontSize="large" />
+                                    </Avatar>
+                                    {coreService.storage.persistent.bgCurrent?.id === bg.id && (
+                                        <SetIcon className={classes.selectIcon} />
+                                    )}
+                                    <Box className={classes.bgActionsWrapper}>
+                                        {coreService.storage.persistent.bgCurrent?.id !== bg.id && (
+                                            <Tooltip title={t('settings.bg.apply')} placement="top">
+                                                <Button className={classes.setIcon} onClick={() => backgroundsService.setCurrentBG(bg.id)}>
+                                                    <SetIcon />
+                                                </Button>
+                                            </Tooltip>
+                                        )}
+                                        <Tooltip title={t('bg.remove')}>
+                                            <IconButton className={classes.deleteIcon} onClick={() => {}}>
+                                                <DeleteIcon />
+                                            </IconButton>
+                                        </Tooltip>
+                                        <Tooltip title={t('settings.bg.openSource')} placement="top">
+                                            <Link
+                                                className={classes.link}
+                                                underline="none"
+                                                href={bg.sourceLink}
+                                                target="_blank"
+                                            >
+                                                <GridListTileBar
+                                                    className={classes.titleBar}
+                                                    classes={{
+                                                        actionIcon: classes.icon,
+                                                    }}
+                                                    subtitle={`by ${bg.author}, Unsplash`}
+                                                    actionIcon={(<LeftIcon />)}
+                                                />
+                                            </Link>
+                                        </Tooltip>
+                                    </Box>
+                                </Box>
+                            </GridListTile>
+                        ))
+                    ])}
+                </GridList>
+            )}
+            {state === FETCH.DONE && bgs.length === 0 && (
                 <FullscreenStub message={t('bg.notFound')} />
             )}
-            {state === 'failed' && (
+            {state === FETCH.FAILED && (
                 <Box className={classes.centerPage}>
                     <Typography variant="h5" color="error">{t('error')}</Typography>
                     <Typography variant="body1" gutterBottom>{t('bg.loadFailed')}</Typography>
                 </Box>
             )}
-        </React.Fragment>
+        </Box>
     );
 }
 
