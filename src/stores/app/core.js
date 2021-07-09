2fc067bcd7e20ecfc409a56fdab859f5f86572d4
Danilkinkin <danilkinkin@gmail.com>
2021-07-09 17:17:37 +0400

    [core] Fixed order initialization

--- src/stores/app/core.js
+++ src/stores/app/core.js
@@ -2,7 +2,8 @@ import BusApp, { initBus } from '@/stores/server/bus';
 import { DESTINATION } from '@/enum';
 import {
     makeAutoObservable,
-    runInAction, toJS,
+    runInAction,
+    toJS,
 } from 'mobx';
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
@@ -12,13 +13,13 @@ import { captureException } from '@sentry/react';
 import BrowserAPI from '@/utils/browserAPI';
 import Storage, { StorageConnector } from '@/stores/universal/storage';
 import awaitInstallStorage from '@/utils/awaitInstallStorage';
+import { forceCrash } from '@/ui/CrashCatch';
 
 const APP_STATE = {
-    WAIT_INIT: 'WAIT_INIT',
+    WAIT: 'WAIT',
     INIT: 'INIT',
     REQUIRE_SETUP: 'REQUIRE_SETUP',
     WORK: 'WORK',
-    FAILED: 'FAILED',
 };
 
 const PREPARE_PROGRESS = {
@@ -35,8 +36,7 @@ class Core {
     globalEventBus;
     localEventBus;
     storage;
-    appState = APP_STATE.WAIT_INIT;
-    appError = '';
+    appState = APP_STATE.WAIT;
     isOffline = !window.navigator.onLine;
 
     constructor({ side }) {
@@ -45,7 +45,7 @@ class Core {
         this.subscribe(side);
     }
 
-    async initialization() {
+    async initI18n() {
         let overrideLng;
 
         if (!PRODUCTION_MODE) {
@@ -56,7 +56,7 @@ class Core {
             overrideLng = devTools.lng;
         }
 
-        await i18n
+        return i18n
             .use(Backend)
             .use(initReactI18next)
             .init({
@@ -87,13 +87,6 @@ class Core {
             })
             .then(() => {
                 console.log('Load i18n!');
-            })
-            .catch((e) => {
-                console.error('Failed init i18n:', e);
-                captureException(e);
-
-                this.appError = 'ERR_INIT_I18N';
-                this.appState = APP_STATE.FAILED;
             });
     }
 
@@ -108,6 +101,23 @@ class Core {
             callback({ type: data });
         });
 
+        window.addEventListener('offline', () => { this.isOffline = true; });
+        window.addEventListener('online', () => { this.isOffline = false; });
+
+        try {
+            console.time('Initialization time');
+            await this.initI18n();
+            console.timeEnd('Initialization time');
+
+            console.log('this.storage.persistent:', toJS(this.storage.persistent.data));
+        } catch (e) {
+            console.error(e);
+            captureException(e);
+
+            forceCrash(new Error('ERR_INIT_I18N'));
+            return;
+        }
+
         console.log('Await sync storage...', this.storage.persistent);
         console.time('Await install storage service');
 
@@ -115,41 +125,20 @@ class Core {
             await awaitInstallStorage(this.storage.persistent);
         } catch (e) {
             console.error(e);
-            this.appError = 'ERR_INIT_STORAGE';
-            this.appState = APP_STATE.FAILED;
+            forceCrash(new Error('ERR_INIT_STORAGE'));
             return;
         }
 
         console.log('Storage is sync!');
         console.timeEnd('Await install storage service');
 
-        try {
-            console.time('Initialization time');
-            await this.initialization();
-            console.timeEnd('Initialization time');
-
-            if (this.appState === APP_STATE.FAILED) throw new Error('Failed init app');
-
-            console.log('this.storage.persistent:', toJS(this.storage.persistent.data));
-
-            runInAction(() => {
-                if (this.storage.persistent.data.factoryResetProgress) {
-                    this.appState = APP_STATE.REQUIRE_SETUP;
-                } else {
-                    this.appState = APP_STATE.WORK;
-                }
-            });
-        } catch (e) {
-            console.error(e);
-            captureException(e);
-            runInAction(() => {
-                this.appError = this.appError || 'ERR_UNKNOWN';
-                this.appState = APP_STATE.FAILED;
-            });
-        }
-
-        window.addEventListener('offline', () => { this.isOffline = true; });
-        window.addEventListener('online', () => { this.isOffline = false; });
+        runInAction(() => {
+            if (this.storage.persistent.data.factoryResetProgress) {
+                this.appState = APP_STATE.REQUIRE_SETUP;
+            } else {
+                this.appState = APP_STATE.WORK;
+            }
+        });
     }
 }
 
