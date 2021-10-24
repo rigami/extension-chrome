dda8b7bf7e3e7f788d904c2cca5088a97db67df5
Danilkinkin <danilkinkin@gmail.com>
2021-10-24 13:51:48 +0400

    [uuid] Added `FIRST_UUID` constant

--- src/utils/generate/uuid.js
+++ src/utils/generate/uuid.js
@@ -1,7 +1,8 @@
 import { v4 } from 'uuid';
 
 const NULL_UUID = '00000000-0000-0000-0000-000000000000';
+const FIRST_UUID = '00000000-0000-0000-0000-000000000001';
 
 export default v4;
 
-export { NULL_UUID, v4 as uuid };
+export { NULL_UUID, FIRST_UUID, v4 as uuid };
