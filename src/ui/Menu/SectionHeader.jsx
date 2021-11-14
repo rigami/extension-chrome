b97725d6802a14c2412a9ef35f86ee113aaa18c1
danilkinkin <danilkinkin@gmail.com>
2021-11-15 00:31:07 +0400

    [SectionHeader] Added `h2` variant

--- src/ui/Menu/SectionHeader.jsx
+++ src/ui/Menu/SectionHeader.jsx
@@ -1,6 +1,7 @@
 import React, { memo } from 'react';
 import { ListSubheader } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -8,13 +9,18 @@ const useStyles = makeStyles((theme) => ({
         fontSize: '22px',
         fontWeight: 800,
     },
+    h1: {},
+    h2: {
+        fontSize: '16px',
+        fontWeight: 800,
+    },
 }));
 
-function SectionHeader({ title }) {
+function SectionHeader({ title, h = 1 }) {
     const classes = useStyles();
 
     return (
-        <ListSubheader inset className={classes.root} disableSticky>
+        <ListSubheader inset className={clsx(classes.root, classes[`h${h}`])} disableSticky>
             {title}
         </ListSubheader>
     );
