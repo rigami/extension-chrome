153fd6f6b8fb97f2e54f694bcab3f82d1f8993ba
danilkinkin <danilkinkin@gmail.com>
2021-12-26 14:08:25 +0400

    [BookmarksGrid] Fixed re-render cards all time

--- src/ui/Bookmarks/BookmarksGrid.jsx
+++ src/ui/Bookmarks/BookmarksGrid.jsx
@@ -1,14 +1,23 @@
 import React, { memo } from 'react';
 import { Box } from '@material-ui/core';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
-import hash from 'object-hash';
 import clsx from 'clsx';
 import { BKMS_VARIANT } from '@/enum';
 import CardLink from '@/ui/Bookmarks/CardLink';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
     root: { width: '100%' },
-    bookmarks: { display: 'flex' },
+    bookmarks: {
+        display: 'flex',
+        position: 'relative',
+    },
+    bookmark: {
+        position: 'absolute',
+        transition: theme.transitions.create(['left', 'top'], {
+            duration: theme.transitions.duration.standard,
+            easing: theme.transitions.easing.easeInOut,
+        }),
+    },
 }));
 
 function BookmarksGrid(props) {
@@ -21,62 +30,66 @@ function BookmarksGrid(props) {
         overloadContent,
     } = props;
     const theme = useTheme();
-    const columnStabilizer = [...Array.from({ length: columns }, () => 0)];
+    const columnStabilizer = Array.from({ length: columns }, () => 0);
     let renderCount = 0;
+    const bookmarksGrid = {};
+    let maxHeight = 0;
+
+    (bookmarks || []).forEach((curr) => {
+        let column = 0;
+        columnStabilizer.forEach((element, index) => {
+            if (
+                columnStabilizer[column] > element
+                && element < maxRows
+            ) column = index;
+        });
+
+        if (columnStabilizer[column] >= maxRows) return;
+
+        bookmarksGrid[curr.id] = {
+            left: column * (theme.shape.dataCard.width + theme.spacing(2)),
+            top: columnStabilizer[column] * (theme.shape.dataCard.height + theme.spacing(2)),
+        };
+
+        columnStabilizer[column] += curr.icoVariant === BKMS_VARIANT.POSTER ? 2 : 1;
+        columnStabilizer[column] += curr.description ? 1 : 0;
+        renderCount += 1;
+
+        const currHeight = Math.max(
+            columnStabilizer[column] * (theme.shape.dataCard.height + theme.spacing(2)) - theme.spacing(2),
+            0,
+        );
+
+        maxHeight = currHeight > maxHeight ? currHeight : maxHeight;
+    }, []);
 
     return (
         <Box className={clsx(classes.root, externalClasses.root)}>
             <Box
-                key={`${bookmarks.length}-${columns}`}
-                display="flex"
+                style={{ height: maxHeight }}
                 className={clsx(classes.bookmarks, externalClasses.bookmarks)}
             >
-                {bookmarks && bookmarks.reduce((acc, curr) => {
-                    let column = 0;
-                    columnStabilizer.forEach((element, index) => {
-                        if (
-                            columnStabilizer[column] > element
-                            && element < maxRows
-                        ) column = index;
-                    });
-
-                    if (columnStabilizer[column] >= maxRows) return acc;
-
-                    columnStabilizer[column] += curr.icoVariant === BKMS_VARIANT.POSTER ? 2 : 1;
-                    columnStabilizer[column] += curr.description ? 1 : 0;
-                    renderCount += 1;
-
-                    if (typeof acc[column] === 'undefined') acc[column] = [];
-
-                    acc[column].push(curr);
-
-                    return acc;
-                }, [])
-                    .map((column, index, arr) => (
-                        <Box
-                            style={{
-                                marginRight: theme.spacing(
-                                    arr.length - 1 !== index ? 2 : 0,
-                                ),
-                            }}
-                            key={hash(column)}
-                        >
-                            {column.map((card) => (
-                                <CardLink
-                                    id={card.id}
-                                    name={card.name}
-                                    url={card.url}
-                                    tags={card.tags}
-                                    tagsFull={card.tagsFull}
-                                    icoVariant={card.icoVariant}
-                                    description={card.description}
-                                    icoUrl={card.icoUrl}
-                                    key={card.id}
-                                    style={{ marginBottom: theme.spacing(2) }}
-                                />
-                            ))}
-                        </Box>
-                    ))}
+                {bookmarks.map((bookmark) => (
+                    <Box
+                        key={bookmark.id}
+                        className={classes.bookmark}
+                        style={{
+                            left: bookmarksGrid[bookmark.id].left,
+                            top: bookmarksGrid[bookmark.id].top,
+                        }}
+                    >
+                        <CardLink
+                            id={bookmark.id}
+                            name={bookmark.name}
+                            url={bookmark.url}
+                            tags={bookmark.tags}
+                            tagsFull={bookmark.tagsFull}
+                            icoVariant={bookmark.icoVariant}
+                            description={bookmark.description}
+                            icoUrl={bookmark.icoUrl}
+                        />
+                    </Box>
+                ))}
             </Box>
             {
                 overloadContent
